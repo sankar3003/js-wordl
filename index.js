0fb4e3f1dc739b2d3aa725e8859c3a3bdbfa8ad3
sankar3003 <57215778+sankar3003@users.noreply.github.com>
2023-02-15 17:41:37 +0530

    learning advanced js

--- index.js
+++ index.js
@@ -6,5 +6,47 @@ const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
 
+var pokemon = {
+  firstname: 'Pika',
+  lastname: 'Chu ',
+  getPokeName: function(c,c1) {
+      var fullname = c + ' ' + c1;
+      return fullname;
+  }
+};
 
- va
+var pokemonName = function() {
+var lastName="rahu"
+  console.log("vfg" + 'I choose you!');
+};
+
+var logPokemon = pokemon.getPokeName.bind({
+  lastName:"SDFfd"
+}); // creates new object and binds pokemon. 'this' of pokemon === pokemon now
+// console.log(pokemon)
+console.log(logPokemon('fsa','dsfa')); // 'Pika Chu I choose you!'
+
+
+//  var person ={
+//     myname :"Goru ",
+//     age :25,
+//     city:"chennai",
+//     getName(name,s1,s2){
+//     console.log(this.myname ,name,s1,s2)
+//     }
+//  }
+// document.getElementById('app').innerHTML="hello sankar"
+//  console.log(document)
+// var user2={
+//   myname:"Raji"
+// }
+
+// var x= person.getName.bind({
+//   myname:"SDF"
+// })
+// x("Aravinfh" , "kumar" , "ajeesh" , "Ragavendra")
+//  person.getName.apply({  myname:"kumar kanna"},["Kuram",'sdf' , 'sdfsd']);
+
+
+
+ 
